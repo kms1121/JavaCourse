5e3a2aade95d26c8baf70f37ed65b8e0fd9edd29
Kyi Min <kyimins0eklpt6822@gmail.com>
2026-07-28 18:31:48 +0630

    learned output text

--- src/chapter1_the_basic/HelloWorld.java
+++ src/chapter1_the_basic/HelloWorld.java
@@ -1,5 +1,11 @@
 package chapter1_the_basic;
 
 public class HelloWorld {
-
+    static void main() {
+        System.out.println("Hello Java");
+        System.out.println(2026);
+        System.out.println(true);
+        System.out.println('$');
+        System.out.println(3.242423523);
+    }
 }
