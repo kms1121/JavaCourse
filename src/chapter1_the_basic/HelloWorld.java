ae51329b7671d628a11fed8f263534229220a23f
Kyi Min <kyimins0eklpt6822@gmail.com>
2026-07-31 18:05:35 +0630

    Revert "adding color text"
    This reverts commit 3b1b51789177e80b9aa5b3037c0f907d75977fda.

--- src/chapter1_the_basic/HelloWorld.java
+++ src/chapter1_the_basic/HelloWorld.java
@@ -8,6 +8,5 @@ public class HelloWorld {
         System.out.println('$');
         System.out.println(3.242423523);
         System.out.println("Hello " + 2026);
-        System.out.println("Color");
     }
 }
