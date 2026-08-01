e3af540b27515c101bd2c057363ab684ea93e1f8
Kyi Min <kyimins0eklpt6822@gmail.com>
2026-08-01 22:07:05 +0630

    declare_Assign in L2

--- src/chapter1_the_basic/Lesson2Variable.java
+++ src/chapter1_the_basic/Lesson2Variable.java
@@ -1,5 +1,7 @@
 package chapter1_the_basic;
 
+import java.util.ArrayList;
+
 public class Lesson2Variable {
     static void main(String[] args) {
         String countryName ="Thailand";
@@ -9,6 +11,9 @@ public class Lesson2Variable {
         System.out.println("I live " + countryName);
         System.out.println("I live " + countryName);
         System.out.println("I live " + countryName);
+
+        countryName = "Germany";
+
         System.out.println("I live " + countryName);
         System.out.println("I live " + countryName);
         System.out.println("I live " + countryName);
@@ -17,5 +22,28 @@ public class Lesson2Variable {
         System.out.println("=====================");
 
         // Variable Initialization
+
+        // Variable declaration // declare
+        int count ;
+
+        //Assign
+        count = 34;
+        System.out.println(count);
+
+        //Java is a static programming language
+        int a = 4;
+
+        // a = "4";
+        System.out.println(a);
+        System.out.println("===========");
+
+        //Type inference
+        var b = "Test";
+        var c =  3;
+        var d = 4.0;
+
+        var list = new ArrayList<Integer>();
+        list.add(3);
+
     }
 }
