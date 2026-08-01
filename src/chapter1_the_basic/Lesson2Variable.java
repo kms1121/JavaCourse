d003d07565565f5633a327d02c2dc89d5e1359f7
Kyi Min <kyimins0eklpt6822@gmail.com>
2026-08-01 21:39:03 +0630

    Lesson2 variable

---
+++ src/chapter1_the_basic/Lesson2Variable.java
@@ -0,0 +1,21 @@
+package chapter1_the_basic;
+
+public class Lesson2Variable {
+    static void main(String[] args) {
+        String countryName ="Thailand";
+
+        System.out.println("I live " + countryName);
+        System.out.println("I live " + countryName);
+        System.out.println("I live " + countryName);
+        System.out.println("I live " + countryName);
+        System.out.println("I live " + countryName);
+        System.out.println("I live " + countryName);
+        System.out.println("I live " + countryName);
+        System.out.println("I live " + countryName);
+        System.out.println("I live " + countryName);
+
+        System.out.println("=====================");
+
+        // Variable Initialization
+    }
+}
