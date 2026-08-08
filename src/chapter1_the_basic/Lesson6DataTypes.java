529a7be9ba072ced343bf924618fe43b364ff7b4
Kyi Min <kyimins0eklpt6822@gmail.com>
2026-08-08 21:51:41 +0630

    Learn Lesson6DataTypes

---
+++ src/chapter1_the_basic/Lesson6DataTypes.java
@@ -0,0 +1,24 @@
+package chapter1_the_basic;
+
+public class Lesson6DataTypes {
+    static void main() {
+        byte b = 127;
+        short s = -13223;
+        int i = 324433434;
+        long l = 3276899955545435645L;
+
+        float f = 1.1234567890123456789f;
+        double d = 2.1234567890123456789;
+
+        System.out.println(f);
+        System.out.println(d);
+
+        char c = 97;
+        System.out.println(c);
+
+        boolean bo = true;
+        System.out.println(bo);
+
+
+    }
+}
